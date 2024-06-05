52b1324d9d0eaa45d31660b7e3958ccea93cc794
baedori99 <baejason36@gmail.com>
2024-06-05 17:57:39 +0900

    Working on 2nd week assignment and study about file I/O in JSON

--- use_api_in_python_own.py
+++ use_api_in_python_own.py
@@ -1,12 +1,41 @@
+import json
+import os
 import urllib.parse
 import urllib.request
-import os
 
-Cl_id = os.getenv("NAVER_CLIENT_ID")
-Cl_secret = os.getenv("NAVER_CLIENT_KEY")
-print(type(Cl_id))
-# encrypt_text = urllib.parse.quote("해리포터")
+from dotenv import load_dotenv
 
-# url = "https://openapi.naver.com/v1/search/kin.json?query=" + encrypt_text
+load_dotenv()
 
 
+def search_naver_news(word):
+
+    client_id = os.getenv("NAVER_CLIENT_ID")
+    client_key = os.getenv("NAVER_CLIENT_KEY")
+    encrypt_text = urllib.parse.quote(word)
+
+    # client_id = str(client_id)
+    # client_key = str(client_key)
+
+    url = (
+        "https://openapi.naver.com/v1/search/shop?query="
+        + encrypt_text
+        + "&display=20&exclude=used:rental"
+    )  # "https://openapi.naver.com/v1/search/news?query=" + encrypt_text
+    req = urllib.request.Request(url)
+
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_key)
+    response = urllib.request.urlopen(req)
+    response_code = response.getcode()
+
+    if response_code == 200:
+        response_output = response.read().decode("utf-8")
+    else:
+        response_output = "Error" + response_code
+
+    return response_output
+
+
+output = search_naver_news("뉴진스")
+print(json.loads(output))
